7b909b0973e2a648c8d6c6a00a0a07d13d14f8bc
muna <muna3828@gmail.com>
2023-12-25 23:31:21 +0600

    created services part

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,15 @@
-
+import img from "../../assets/businessman-consulting-watch_74855-2350.jpg"
 
 const About = () => {
     return (
         <div>
-            about us
+            <h1 className="text-3xl font-bold py-10 text-center">About Us</h1>
+            <div >
+                <img src={img} alt="" className="w-1/2 mx-auto" />
+                <p className="w-2/3 py-20 text-xl mx-auto">Welcome to TaskBurst, where productivity meets simplicity. We understand that managing tasks can be challenging, whether you're an individual striving for personal efficiency or a team working towards collective success.
+
+                    At [Your Website Name], we believe in empowering you to take control of your tasks and projects with ease. Our platform is designed to simplify your workflow, enhance collaboration, and bring clarity to your daily responsibilities. With an intuitive interface and powerful features, we strive to make task management a seamless and enjoyable experience for everyone.</p>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import img from "../../assets/banner.jpg"
 import './Home.css'
+import { FaPencilAlt } from 'react-icons/fa';
 
 const Home = () => {
     return (
@@ -14,6 +15,38 @@ const Home = () => {
                         <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                         <Link to={"/login"}><button className="btn btn-primary">Let’s Explore</button></Link>
                     </div>
+                </div>
+            </div>
+            {/* service */}
+            <div>
+                <h1 className="text-3xl font-bold py-10 text-center">Our Services</h1>
+                <div className='lg:flex gap-10 px-20 pb-20'>
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <div className="card-body">
+                            <h2 className="card-title text-center text-2xl">For Developers</h2>
+                            <p className='text-blue-600 font-bold'>Code Faster, Collaborate Better with TaskBurst</p>
+                            <p>TaskBurst streamlines your coding workflow. Customize task boards, integrate version control, and collaborate in real-time. Elevate your development game with TaskBurst.
+                            </p>
+
+                        </div>
+                    </div>
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <div className="card-body">
+                        <h2 className="card-title text-center text-2xl">For Corporate Professionals</h2>
+                            <p className='text-blue-600 font-bold'>Boost Corporate Excellence with TaskBurst</p>
+                            <p>Stay organized and succeed in the corporate world. TaskBurst offers powerful project management, team collaboration, and tracking features. Your go-to solution for corporate efficiency.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <div className="card-body">
+                        <h2 className="card-title text-center text-2xl">For Bankers</h2>
+                            <p className='text-blue-600 font-bold'>Precision in Finance with TaskBurst</p>
+                            <p>TaskBurst is crafted for financial precision. Manage critical deadlines, coordinate reports, and ensure secure collaboration. Experience a new level of financial efficiency with TaskBurst.
+                            </p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
